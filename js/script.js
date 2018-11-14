c1f652994333fb0311a7a5e50b04e3c290e6a9d6
Tony Huang <tonyhuang4326@gmail.com>
2018-11-14 02:15:35 +0000

    added function answerLength

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 /* global $ */
 
 $(document).ready(function() {
+    
+    function answerLength(a1,a2,a3){
+        var score1= a1.length;
+        var score2= a2.length;
+        var score3= a3.length;
+        return score1 + score2 + score3;
+    }
+    
     $("button").click(function() {
         var name = $("#name").val();
         var techPlacement = "TBD";
@@ -8,7 +16,7 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var totalScore = answerLength(q1Result,q2Result,q2Result);
     });
 
 });
