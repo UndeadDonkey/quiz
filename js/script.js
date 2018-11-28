0cd0d47716109dc646a0c61e9fcfcbea8763234d
Tony Huang <tonyhuang4326@gmail.com>
2018-11-28 02:06:14 +0000

    fixed js and added conditional statement

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
         return score1 + score2 + parseInt(score3,10);
     }
     $("button").click(function() {
-        console.log("works");
         var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
@@ -17,37 +16,36 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore = answerLength(q1Result, q2Result, q3Result);
-        console.log("this is work",totalScore);
-        if(totalScore < 2.2) {
+        if(totalScore >=8 && totalScore < 10) {
             $("final").text("sadasdasdas");
         }
-        else if(totalScore < 4.4 && totalScore >= 2.2) {
+        else if(totalScore >=10 && totalScore < 12) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 6.6 && totalScore >= 4.4) {
+        else if(totalScore >=12 && totalScore < 14) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 8.8 && totalScore >= 6.6) {
+        else if(totalScore >=14 && totalScore < 16) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 11 && totalScore >= 8.8) {
+        else if(totalScore >=16 && totalScore < 18) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 13.2 && totalScore >= 11) {
+        else if(totalScore >=18 && totalScore < 20) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 15.4 && totalScore >= 13.2) {
+        else if(totalScore >=20 && totalScore < 22) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 17.6 && totalScore >= 15.4) {
+        else if(totalScore >=8 && totalScore < 10) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore < 19.8 && totalScore >= 17.6) {
+        else if(totalScore >=8 && totalScore < 10) {
             $("#final").text("sadasdasdas");
         }
-        else if(totalScore <= 22 && totalScore >= 19.8) {
+        else if(totalScore >=8 && totalScore < 10) {
             $("#final").text("sadasdasdas");
-        }else{
+        }else if{
             $("#final").text("sadasdasdas");
         }
     });
