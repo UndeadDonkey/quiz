5c953fb0f102a2cd221dd2dff39a8f0bd51840e1
Tony Huang <tonyhuang4326@gmail.com>
2018-12-12 01:07:48 +0000

    Fixed conditionals

--- js/script.js
+++ js/script.js
@@ -16,37 +16,38 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore = answerLength(q1Result, q2Result, q3Result);
-        if(totalScore >=8 && totalScore < 10) {
-            $("final").text("sadasdasdas");
+        console.log(totalScore);
+        if(totalScore >= 8 && totalScore < 10) {
+            $("#final").text("Big Pear");
         }
         else if(totalScore >=10 && totalScore < 12) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Evil Pear");
         }
         else if(totalScore >=12 && totalScore < 14) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Cold Pear");
         }
         else if(totalScore >=14 && totalScore < 16) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Cooked Pear");
         }
         else if(totalScore >=16 && totalScore < 18) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Pear with a door");
         }
         else if(totalScore >=18 && totalScore < 20) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Giant Pear");
         }
         else if(totalScore >=20 && totalScore < 22) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Sad Pear");
         }
         else if(totalScore >=8 && totalScore < 10) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Good Pear");
         }
         else if(totalScore >=8 && totalScore < 10) {
-            $("#final").text("sadasdasdas");
+            $("#final").text("Troll Pear");
         }
         else if(totalScore >=8 && totalScore < 10) {
-            $("#final").text("sadasdasdas");
-        }else if{
-            $("#final").text("sadasdasdas");
+            $("#final").text("Normal Pear");
+        }else {
+            $("#final").text("Small Pear");
         }
     });
 });
